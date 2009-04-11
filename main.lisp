dac772d576ac522ec2c296f3f7ec1a18ecd301d8
Zekeriya KOC <zekzekus@gmail.com>
2009-04-11 23:38:09 +0300

    some actions implemented.

--- main.lisp
+++ main.lisp
@@ -11,12 +11,31 @@
 ;; requested made http://localhost:8080/ will invokde hello-world function.
 (push (create-regex-dispatcher "^/$" 'hello-world) *dispatch-table*)
 
+;; adding dispatchers for increment and decrement operations.
+(push (create-regex-dispatcher "^/inc$" 'incval) *dispatch-table*)
+(push (create-regex-dispatcher "^/dec$" 'decval) *dispatch-table*)
+
+;; a global variable to increment and decrement.
+(defvar *my-var* 0)
+
 ;; it's time to say "Hello World!!!"
 (defun hello-world ()
-  (with-html-output (*standard-output* nil :indent t)
+  (with-html-output-to-string (*standard-output* nil :indent t)
     (:html
      (:head
       (:title "Page: Hello-World"))
      (:body
       (:h2 "Hello World!!")
-      (:hr)))))
+      (:hr)
+      (fmt "The Value: ~S --->" *my-var*)
+      (:a :href "/inc" "++")
+      (str "&nbsp")
+      (:a :href "/dec" "---")))))
+
+(defun incval ()
+  (incf *my-var*)
+  (redirect "/"))
+
+(defun decval ()
+  (decf *my-var*)
+  (redirect "/"))
