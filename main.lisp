9c1e1df2c6d9de93bd2272e050b93c47f57e1c9f
Zekeriya KOC <zekzekus@gmail.com>
2009-04-11 20:13:48 +0300

    hello world level
    added a dispatcher and function to show how to say hello world.

--- main.lisp
+++ main.lisp
@@ -1,1 +1,22 @@
 (in-package :hunc-exp)
+
+;; first define our web server on port 8080.
+(defvar *server* (make-instance 'acceptor :port 8080))
+
+;; then a initialization function to start it!!
+(defun init ()
+  (start *server*))
+
+;; add a dispatcher for our hello-world function. this means every
+;; requested url starts with /hello-world will invokde hello-world function.
+(push (create-regex-dispatcher "^/hello-world$" 'hello-world) *dispatch-table*)
+
+;; it's time to say "Hello World!!!"
+(defun hello-world ()
+  (with-html-output (*standard-output* nil :indent t)
+    (:html
+     (:head
+      (:title "Page: Hello-World"))
+     (:body
+      (:h2 "Hello World!!")
+      (:hr)))))
